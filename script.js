24b1748503b5b9078beb1ebd114204a0bfda4877
marcos <munoz.marcos.epet20@gmail.com>
2022-10-13 12:45:47 -0300

    var alumno = "Marcos Muñoz"; var matematica = "7"; var lengua = "9"; var taller = "8"; function promalm(matematica, lengua, taller){      sumas = matematica + lengua + taller;     prom = sumas/3 } console.log(promalm);

---
+++ script.js
@@ -0,0 +1,9 @@
+var alumno = "Marcos Muñoz";
+var matematica = "7";
+var lengua = "9";
+var taller = "8";
+function promalm(matematica, lengua, taller){
+     sumas = matematica + lengua + taller;
+    prom = sumas/3 
+}
+console.log(promalm);
